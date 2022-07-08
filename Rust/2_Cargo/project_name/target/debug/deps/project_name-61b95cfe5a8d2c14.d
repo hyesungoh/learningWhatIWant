2173a7688c99e3366e51d67785ac4d21ccf4b3fa
Hyesung Oh <haesungoh414@gmail.com>
2022-07-08 22:06:24 +0900

    update: rust cargo project

---
+++ Rust/2_Cargo/project_name/target/debug/deps/project_name-61b95cfe5a8d2c14.d
@@ -0,0 +1,5 @@
+/Users/hyesungoh/Documents/learningFrontEnd/Rust/2_Cargo/project_name/target/debug/deps/project_name-61b95cfe5a8d2c14: src/main.rs
+
+/Users/hyesungoh/Documents/learningFrontEnd/Rust/2_Cargo/project_name/target/debug/deps/project_name-61b95cfe5a8d2c14.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/2_Cargo/project_name/target/debug/project_name.d
@@ -0,0 +1,1 @@
+/Users/hyesungoh/Documents/learningFrontEnd/Rust/2_Cargo/project_name/target/debug/project_name: /Users/hyesungoh/Documents/learningFrontEnd/Rust/2_Cargo/project_name/src/main.rs
